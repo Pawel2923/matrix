3274d6b12c7f59a2ae056a4dd9171498d42d2a6e
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-12-06 00:34:11 +0100

    /przeciążeniowy alokuje pamięć i przepisuje dane z tabeli,

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -7,3 +7,16 @@ Matrix::Matrix(int n) {
         matrix[i] = new int[n];
     }
 }
+Matrix::Matrix(int n, int* t) {
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = t[k];
+            k++;
+        }
+    }
+}
